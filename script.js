02ed1c7b0f9d3a31726bb0cdd17b1d1d14594451
Aayush Bista <aayushbista70@gmail.com>
2026-03-22 08:54:16 +0545

    added js

---
+++ script.js
@@ -0,0 +1,30 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.className = "task-item";
+
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <button>Delete</button>
+  `;
+
+  li.querySelector("button").addEventListener("click", () => {
+    li.remove();
+  });
+
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
+
+addTaskBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") addTask();
+});
